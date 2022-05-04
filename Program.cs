bed219a4abdf8e18f14aea046a6196b0cc02cae6
Antonin drujon <toto@enterprise.com>
2022-05-04 15:02:23 +0200

    création du programme Armstrong

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool Armstrong(int nombre)
+{
+    int c ,d , u;
+    c = (nombre/100);
+    d = ((nombre%100)/10);
+    u = ((nombre%100)%10);
+    int cal = (c*c*c) + (d*d*d) + (u*u*u);
+    if(cal == nombre)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Console.WriteLine(Armstrong(153));
+
+
